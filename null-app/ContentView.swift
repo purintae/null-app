436e72bf829372d7d80821bb0830f82e54988913
purintae <purin.tae1997@gmail.com>
2026-08-12 10:41:02 +0700

    Initial Commit

---
+++ null-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  null-app
+//
+//  Created by Purin Tae on 12/8/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ null-app/null_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  null_appApp.swift
+//  null-app
+//
+//  Created by Purin Tae on 12/8/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct null_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
